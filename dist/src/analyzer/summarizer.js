e64fa473c9afac6049645f10a1c5d01b899b8441
nirvik34 <jarhnirvik@gmail.com>
2026-02-15 16:52:41 +0530

    chore: remove node_modules and dist from repository

--- dist/src/analyzer/summarizer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateSummary = generateSummary;
-function generateSummary(files) {
-    const summaries = files.map(file => {
-        return `${file.path} (+${file.additions} - ${file.deletions})`;
-    });
-    return summaries.join("\n");
-}
--- dist/src/config/loadConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.loadConfig = loadConfig;
-const cosmiconfig_1 = require("cosmiconfig");
-async function loadConfig() {
-    const explorer = (0, cosmiconfig_1.cosmiconfig)("smartcommit");
-    const result = await explorer.search();
-    return result?.config || {};
-}
